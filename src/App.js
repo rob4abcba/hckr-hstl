8a1dd8fdbc03e18ad7d8c87c9994a9016205bde4
rob4abcba <rob_abcba@yahoo.com>
2019-09-20 13:19:15 -0700

    Create state hackers arr. Pass prop to Error Meals

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import Bookings from './components/Bookings';
 import Meals from './components/Meals';
 import Error from './components/Error';
 
-
 class App extends Component {
+    state = {
+        hackers: [
+            {
+                id: 'tyler',
+                name: 'Tyler McGinnis',
+                handle: '@tylermicginnis',
+                avatarURL: 'http://localhost:5001/tyler.jpg'
+            },
+            {
+                id: 'karen',
+                name: 'Karen McGinnis',
+                handle: '@karenmicginnis',
+                avatarURL: 'http://localhost:5001/karen.jpg'
+            },
+        ]
+    }
+
+    createHacker(hacker) {
+        // HackersAPI.create(hacker).then(hacker => {
+        //     this.setState(state => ({
+        //         hackers: state.hackers.concat([ hacker ])
+        //     }))
+        // })
+    }
 
     render() {
         return (<div className="container-fluid">
             <center>
-                <h2>Hacker Hostel</h2>
+                <h2>Hacker Yo Yo Hostel</h2>
             </center>
             <div className="container">
                 <Bookings></Bookings>
-                <Error></Error>
-                <Meals></Meals>
+                <Error hackers={this.state.hackers}></Error>
+                <Meals hackers={this.state.hackers}></Meals>
             </div>
         </div>);
     }
