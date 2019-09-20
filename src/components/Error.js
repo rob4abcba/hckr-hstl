d9fe5050855d889cf7b9ec1898d65c6d68df5375
rob4abcba <rob_abcba@yahoo.com>
2019-09-20 13:24:27 -0700

    Func Child comp receiv prop hackers arr. Map 2 UI

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
-const Error = (() => {
+const Error = ((props) => {
     return (<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 error">
             <div id="list">
             <div className="error-msg">
                     <i className="fa fa-times-circle"></i>
-            <p>Error! No menu generated for insert_hacker_name</p>
+                    {props.hackers.map((hacker) => (<p> Error! No menu generated for {hacker.name} </p>))}
+            {/* <p>Error! No menu generated for insert_hacker_name</p> */}
                     </div>
             </div>
         </div>);
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
-const Meals = (() => {
+const Meals = ((props) => {
     return (<div className="col-xs-12  col-sm-12 col-md-12 col-lg-12">
             <ol id="list">
             <div>
-                        <li className="morning">Breakfast for insert_hacker_name</li>
-                        <li className="afternoon">Lunch for insert_hacker_name</li>
-                        <li className="night">Dinner for insert_hacker_name</li>
+                        <li className="morning">
+                            {/* <div
+                            className="am-pm-night-avatar"
+                            style={{
+                                backgroundImage: `url(${props.hacker.avatarURL})`
+                            }}>
+                            </div> */}
+                            <div className='hacker-details'>
+                                {/* {props.hackers.map((hacker) => (<p> `Breakfast for ${hacker.name}`</p>))} */}
+                                {props.hackers.map((hacker) => (<p> Breakfast for {hacker.name} </p>))} 
+                            </div>
+                            </li>
+
+                        <li className="afternoon">
+                            Lunch for insert_hacker_name
+                            </li>
+                        <li className="night">
+                            Dinner for insert_hacker_name
+                            </li>
                     </div>
             </ol>
         </div>);
